c2e97945a7eb4c19851dad0279c6876380d57ba4
Yangyang Fu <fuyy2008@gmail.com>
2025-06-27 00:41:15 -0500

    update code based on latest remote

--- cases/qlora_vla.sh
+++ cases/qlora_vla.sh
@@ -3,7 +3,7 @@ torchrun \
 --standalone \
 --nnodes 1 \
 --nproc-per-node 1 \
-scripts/finetune_genie.py \
+scripts/finetune.py \
 --vla_path ckpts/univla-7b \
 --lam_path ckpts/univla-latent-action-model/lam-stage-2.ckpt \
 --data_root_dir dataset/Manipulation-SimData \
@@ -12,11 +12,12 @@ scripts/finetune_genie.py \
 --grad_accumulation_steps 4 \
 --max_steps 300000 \
 --save_steps 1000 \
---learning_rate 1e-4 \
+--learning_rate 1e-5 \
 --run_root_dir runs/agibot \
 --adapter_tmp_dir runs/agibot \
 --use_lora \
 --use_quantization \
 --wandb_project qlora_vla \
 --with_proprio \
+--wogripper \
 --task_ids 1
